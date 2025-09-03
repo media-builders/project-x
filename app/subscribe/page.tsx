dd29b4667da1b7b7c21d1d1e91fa74c2746b4b35
media-builders <nadeem@mediabuilders.ca>
2025-09-03 07:25:52 +0000

    logout button

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,3 +1,5 @@
+// app/subscribe/page.tsx (add these two lines)
+import LogoutButton from "@/components/LogoutButton";
 import StripePricingTable from "@/components/StripePricingTable";
 import Image from "next/image"
 import { createClient } from '@/utils/supabase/server'
@@ -18,6 +20,9 @@ export default async function Subscribe() {
             <div className="w-full py-20 lg:py-32 xl:py-40">
                 <div className="text-center py-6 md:py-10 lg:py-12 ">
                     <h1 className="font-bold text-xl md:text-3xl lg:text-4xl ">Pricing Test</h1>
+                    <div className="flex justify-end mb-4">
+                        <LogoutButton />
+                    </div>
                     <h1 className="pt-4 text-muted-foreground text-sm md:text-md lg:text-lg">Choose the right plan for your team! Cancel anytime!</h1>
                 </div>
                 <StripePricingTable checkoutSessionSecret={checkoutSessionSecret} />
