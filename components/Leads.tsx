5321827090882c2398f2be34822bc65d92b4d376
media-builders <nadeem@mediabuilders.ca>
2025-09-11 07:01:42 +0000

    lead table ui

--- components/Leads.tsx
+++ components/Leads.tsx
@@ -2,6 +2,25 @@
 
 export default function LeadsTable() {
   return (
-    <div><h1>Leads</h1></div>
+    <table>
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+      <th>Phone Number</th>
+    </tr>
+    <tr>
+      <td>Jane</td>
+      <td>Doe</td>
+      <td>jane.doe@example.com</td>
+      <td>(555) 123-4567</td>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Smith</td>
+      <td>john.smith@example.com</td>
+      <td>(555) 987-6543</td>
+    </tr>
+  </table>
   );
 }
