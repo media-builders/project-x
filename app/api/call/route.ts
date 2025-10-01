97396f8be64e751c5848bf6258cf6c6cc96de3b1
Rania Alvi <rania.alvi@gmail.com>
2025-10-01 06:51:59 -0400

    fixed migration errors

---
+++ app/api/call/route.ts
@@ -0,0 +1,3 @@
+import { ElevenLabsClient } from "@elevenlabs/elevenlabs-js";
+import { db } from "@/utils/db/db";
+import { usersTable, } from "@/utils/db/schema";
--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp , bigint} from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users_table', {
     id: text('id').primaryKey(),
@@ -6,8 +6,24 @@ export const usersTable = pgTable('users_table', {
     email: text('email').notNull().unique(),
     plan: text('plan').notNull(),
     stripe_id: text('stripe_id').notNull(),
-    crm_api_key: text('crm_api_key')
+    crm_api_key: text('crm_api_key'),
+    twilio_number: text('twilio_number')
+});
+
+export const leadsTable = pgTable("leads", {
+  id: text("id").primaryKey(),
+  user_id: text("user_id").notNull(),         
+  fub_id: bigint("fub_id", { mode: "number" }),       
+  first: text("first").notNull(),
+  last: text("last").notNull(),
+  email: text("email"),
+  phone: text("phone"),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updated_at").defaultNow().notNull(),
 });
 
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
+
+export type InsertLead = typeof leadsTable.$inferInsert;
+export type SelectLead = typeof leadsTable.$inferSelect;
